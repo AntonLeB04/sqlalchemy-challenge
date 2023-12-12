96a62334f4e616053ff3146f85ec83235195429e
Anton LeBeque <144728558+AntonLeB04@users.noreply.github.com>
2023-12-12 11:56:31 -0600

    Update app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,98 @@
 # Import the dependencies.
-
-
-
+from flask import Flask, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy import func
+import numpy as np
+import pandas as pd
+import datetime as dt
 #################################################
 # Database Setup
 #################################################
-
-
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite", echo=False)
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Hawaii Precipitation API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs"
+    )
+
+# will return a list of the dates and the precipitation on those dates
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+
+    recent_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
+
+    year_ago_date = dt.date(2017, 8, 23) - dt.timedelta(days= 365)
+
+    prec_data = session.query(measurement.date, measurement.prcp).order_by(measurement.date.asc()).\
+    filter(measurement.date >= year_ago_date).all()
+
+    session.close()
+
+    precipitation = []
+#gathering the dates and the precipitation for the last year
+    for date, prcp in prec_data:
+        precipitation_dict = {}
+        precipitation_dict[date] = date
+        precipitation_dict[prcp] = prcp
+        precipitation.append(precipitation_dict)
+    return jsonify(precipitation)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+
+    stations = session.query(station.station, station.name, station.latitude, station.longitude, station.elevation).all()
+    session.close()
+#gathers all the station information and puts them into a dictionary
+    station_info = []
+    for station, name, latitude, longitude, elevation in stations:
+        station_dict = {}
+        station_dict["station"] = station
+        station_dict["name"] = name
+        station_dict["latitude"] = latitude
+        station_dict["longitude"] = longitude
+        station_dict["elevation"] = elevation
+        station_info.append(station_dict).all()
+    return jsonify(station_info)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+@app.route("/api/v1.0/<start>")
+def start():
+
+@app.route("/api/v1.0/<start>/<end>")
+def end():
+
+if __name__ == "__main__":
+    app.run(debug=True)

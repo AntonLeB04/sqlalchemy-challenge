bd21c2e9319dadcbdf68ce4ff171b2fb1454d741
Anton LeBeque <144728558+AntonLeB04@users.noreply.github.com>
2023-12-12 16:35:19 -0600

    Update app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -11,7 +11,7 @@ import datetime as dt
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///../Resources/hawaii.sqlite", echo=False)
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", echo=False)
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
@@ -46,8 +46,6 @@ def welcome():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
-
-    recent_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
 
     year_ago_date = dt.date(2017, 8, 23) - dt.timedelta(days= 365)
 
@@ -87,12 +85,26 @@ def stations():
 def tobs():
     session = Session(engine)
 
+    year_ago_date = dt.date(2017, 8, 23) - dt.timedelta(days= 365)
+    #getting the top stations information
+    station_info = session.query(measurement.date, measurement.tobs).order_by(measurement.date.asc()).\
+                            filter(measurement.date >= year_ago_date).\
+                            filter(measurement.station == 'USC00519281').all()
+    session.close()
 
-@app.route("/api/v1.0/<start>")
-def start():
+    tobs_one_year = []
+    for date, tobs in station_info:
+        tobs_dict = {}
+        tobs_dict['date'] = date
+        tobs_dict['tobs'] = tobs
+        tobs_one_year.append(tobs_dict)
+    return jsonify(tobs_one_year)
 
-@app.route("/api/v1.0/<start>/<end>")
-def end():
+#@app.route("/api/v1.0/<start>")
+#def start():
+
+#@app.route("/api/v1.0/<start>/<end>")
+#def end():
 
 if __name__ == "__main__":
     app.run(debug=True)
